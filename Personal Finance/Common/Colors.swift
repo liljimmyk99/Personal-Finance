78bdff933984c548e86164623506d894e548b964
Jimmy Kane <liljimmykane@comcast.net>
2025-06-29 20:54:54 -0400

    feat: Created initial color palette

--- Personal Finance/Common/Colors.swift
+++ Personal Finance/Common/Colors.swift
@@ -7,5 +7,14 @@
 import SwiftUI
 
 struct PFColors {
-    static var primary: Color  = Color("PFPrimary", bundle: .main)
+    static let background: Color  = Color("PFBackground", bundle: .main)
+    static let destructiveButton: Color  = Color("PFDestructiveButton", bundle: .main)
+    static let headingText: Color  = Color("PFHeadingText", bundle: .main)
+    static let mainText: Color  = Color("PFMainText", bundle: .main)
+    static let primary: Color  = Color("PFPrimary", bundle: .main)
+    static let signIn: Color  = Color("PFSignIn", bundle: .main)
+    static let surface: Color  = Color("PFSurface", bundle: .main)
+    static let textFieldActive: Color  = Color("PFTextFieldActive", bundle: .main)
+    static let textFieldDisabled: Color  = Color("PFTextFieldDisabled", bundle: .main)
+    static let textFieldStatic: Color  = Color("PFTextFieldStatic", bundle: .main)
 }
