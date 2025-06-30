e84458c2d7c06e6460b0a1a92bef6d3818548ff0
Jimmy Kane <liljimmykane@comcast.net>
2025-06-29 21:05:36 -0400

    fix: kanbanCard rounded corners

--- Personal Finance/Views/Reports/Category/Components/KanbanCardView.swift
+++ Personal Finance/Views/Reports/Category/Components/KanbanCardView.swift
@@ -27,7 +27,7 @@ struct KanbanCard: View {
                         HStack {
                             Text(store)
                                 .font(.title2)
-                                .foregroundStyle(Color.green)
+                                .foregroundStyle(PFColors.headingText)
                             
                             Spacer()
                         }
@@ -36,27 +36,28 @@ struct KanbanCard: View {
                         
                         HStack {
                             Text(date.formatted(.dateTime.month().day().year()))
-                                .foregroundStyle(Color.green)
+                                .foregroundStyle(PFColors.mainText)
                             
                             Spacer()
                             
                             Text(amount, format: .currency(code: "USD"))
                                 .font(.headline)
-                                .foregroundStyle(Color.green)
+                                .foregroundStyle(PFColors.mainText)
                         }
                         
                         Spacer()
                     }
                     .padding()
-                    .background(Color.gray)
+                    .background(PFColors.surface)
                 }
-                .overlay(
-                    // Overlay a new RoundedRectangle and stroke it
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.green, lineWidth: 5)
-                )
         }
         .buttonStyle(PlainButtonStyle())
+        .overlay(
+            // Overlay a new RoundedRectangle and stroke it
+            RoundedRectangle(cornerRadius: 5)
+                .stroke(PFColors.primary, lineWidth: 5)
+        )
+        .padding()
     }
 }
 
